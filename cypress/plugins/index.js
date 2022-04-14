068ea2d541083d63b20fc057175689462b18f007
Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>
2022-04-14 19:28:36 +0300

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,24 +1,8 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
 
 const faker = require("faker");
 const { clear } = require("../../server/db");
+const { seed } = require("../../server/db");
 
 module.exports = (on, config) => {
   on("task", {
@@ -40,6 +24,11 @@ module.exports = (on, config) => {
     },
     'db:clear'() {
       clear();
+
+      return null;
+    },
+    'db:seed'() {
+      seed();
 
       return null;
     }
